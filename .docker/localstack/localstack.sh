708633bf8902ff04c5f6dafb805d94d2c3ea2aaf
Vlad Marian <transilvlad@gmail.com>
2025-07-29 22:13:31 +0200

    Add docker compose for localstack with S3, SQS and DynamoDB

---
+++ .docker/localstack/localstack.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Initializing localstack"
+
+awslocal s3 mb s3://email-attachments
+awslocal sqs create-queue --queue-name email-processing
+awslocal dynamodb create-table \
+             --table-name received \
+             --attribute-definitions \
+                 AttributeName=Subject,AttributeType=S \
+                 AttributeName=MessageID,AttributeType=S \
+                 AttributeName=Sender,AttributeType=S \
+                 AttributeName=Recipient,AttributeType=S \
+             --key-schema AttributeName=Subject,KeyType=HASH AttributeName=MessageID,KeyType=RANGE \
+             --billing-mode PAY_PER_REQUEST \
+             --table-class STANDARD
