9a7f0e6a1406406470c08e2f7cf9f6843f5f4f5a
Vlad Marian <transilvlad@gmail.com>
2025-07-30 19:05:50 +0200

    Update lambda_function_test.py test_event

--- localstack/lambda_function_test.py
+++ localstack/lambda_function_test.py
@@ -6,14 +6,22 @@ from localstack.lambda_function import lambda_handler
 if __name__ == "__main__":
     # Sample test event
     test_event = {
-        "Records": [
+        'Records': [
             {
-                "messageId": "test-message-id",
-                "body": json.dumps(
-                    {'sender': 'robin@transilvlad.net', 'recipient': 'robin@example.com', 'raw_message': '',
-                     'domain': 'example.com', 'subject': 'Lipsum',
-                     'message_id': '<557828a4-7624-41bc-9f4e-c3a83b5babfe-1753893239239robin@transilvlad.net>',
-                     'timestamp': 1753893239.0})
+                'messageId': '7cdb2a87-7578-490e-b62f-c1a0c50d6238',
+                'receiptHandle': 'OTA3NjNhOTAtZDZkOC00YzA1LTk5ODAtOWRkNjcyZGY1YTNmIGFybjphd3M6c3FzOnVzLWVhc3QtMTowMDAwMDAwMDAwMDA6ZW1haWwtcHJvY2Vzc2luZyA3Y2RiMmE4Ny03NTc4LTQ5MGUtYjYyZi1jMWEwYzUwZDYyMzggMTc1Mzg4NTQ4Ny42OTg3MDY=',
+                'body': '{"sender": "robin@transilvlad.net", "recipient": "lady@example.com", "raw_message": "", "domain": "example.com", "subject": "Lipsum", "message_id": "<0eb7620e-131a-46f9-ac2b-305ee9e3fce5-1753885487145robin@transilvlad.net>", "timestamp": 1753885487.0}',
+                'attributes': {
+                    'SenderId': '000000000000',
+                    'SentTimestamp': '1753885487693',
+                    'ApproximateReceiveCount': '1',
+                    'ApproximateFirstReceiveTimestamp': '1753885487698'
+                },
+                'messageAttributes': {},
+                'md5OfBody': '391f22a7ccff24ee6d5e9c6d795c33bc',
+                'eventSourceARN': 'arn:aws:sqs:us-east-1:000000000000:email-processing',
+                'eventSource': 'aws:sqs',
+                'awsRegion': 'us-east-1'
             }
         ]
     }
