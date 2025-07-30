0bbb8b19011cfde75691e6ea3b3da7a43a9d6f04
Vlad Marian <transilvlad@gmail.com>
2025-07-30 13:03:00 +0200

    Add lambda function and refactor manager.py location for future use

--- .docker/localstack/start-localstack.sh
+++ .docker/localstack/start-localstack.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
+cd /app || exit
+
 echo "Initializing localstack"
 
 awslocal s3 mb s3://email-attachments
+
 awslocal sqs create-queue --queue-name email-processing
+
+zip lambda_function.zip lambda_function.py
+
+awslocal lambda create-function \
+            --function-name email_processor \
+            --runtime python3.13 \
+            --zip-file fileb://lambda_function.zip \
+            --handler lambda_function.lambda_handler \
+            --role arn:aws:iam::000000000000:role/lambda-role
+
 #awslocal dynamodb create-table \
-#             --table-name received \
-#             --attribute-definitions \
+#            --table-name received \
+#            --attribute-definitions \
 #                 AttributeName=Subject,AttributeType=S \
 #                 AttributeName=MessageID,AttributeType=S \
 #                 AttributeName=Sender,AttributeType=S \
 #                 AttributeName=Recipient,AttributeType=S \
-#             --key-schema AttributeName=Subject,KeyType=HASH AttributeName=MessageID,KeyType=RANGE \
-#             --billing-mode PAY_PER_REQUEST \
-#             --table-class STANDARD
+#            --key-schema AttributeName=Subject,KeyType=HASH AttributeName=MessageID,KeyType=RANGE \
+#            --billing-mode PAY_PER_REQUEST \
+#            --table-class STANDARD
